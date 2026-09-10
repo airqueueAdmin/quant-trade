ff5789b17e33b689c8019a0f36f500fdea0b554b
airQueueAdmin <airqueue0309@gmail.com>
2026-09-10 09:48:19 +0900

    chore: migrate Toss in-app SDK to v3

--- toss-inapp/apps-in-toss.config.ts
+++ toss-inapp/apps-in-toss.config.ts
@@ -3,21 +3,9 @@ import { defineConfig } from '@apps-in-toss/web-framework/config'
 export default defineConfig({
   appName: 'glance-invest',
   brand: {
-    displayName: '한눈투자',
     primaryColor: '#2f6bff',
-    icon: 'https://static.toss.im/appsintoss/55493/7e45fd4d-c4f7-4991-a390-f815534f9719.png',
   },
-  web: {
-    host: 'localhost',
-    port: 5173,
-    commands: {
-      dev: 'vite',
-      build: 'vite build',
-    },
-  },
-  webViewProps: {
-    type: 'partner',
-  },
+  webView: {},
   navigationBar: {
     withBackButton: true,
     withHomeButton: false,
@@ -25,5 +13,5 @@ export default defineConfig({
     transparentBackground: false,
   },
   permissions: [],
-  outdir: 'dist',
+  webBundleDir: 'dist',
 })
--- toss-inapp/vite.config.ts
+++ toss-inapp/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import aitDevtools from '@apps-in-toss/devtools/unplugin'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [aitDevtools.vite(), react()],
   server: {
     host: '0.0.0.0',
     port: 5173,
